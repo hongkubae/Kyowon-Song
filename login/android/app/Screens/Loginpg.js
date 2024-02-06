70b4fab9c970457c4b8cbf9b00546e297e77a8ae
GyowonS <152247242+GyowonS@users.noreply.github.com>
2024-02-07 07:43:31 +0900

    Update Loginpg.js

--- login/android/app/Screens/Loginpg.js
+++ login/android/app/Screens/Loginpg.js
@@ -12,79 +12,120 @@ import Eye from "../assets/icons/Eye";
 import TruffleLogo from "../assets/logo/TruffleLogo";
 //import useAuth from '../BackFunc/AuthFunc';
 
-function Loginpg({ navigation, setIsLoggedIn }) {
-  //---email, pw, login여부 변수 지정---\\
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  //const { isLoggedIn, handleLogin } = useAuth();
+function Loginpg({ navigation }) {
+const [password, setPassword] = useState('');
+const [email, setEmail] = useState('');
+const [validation, setValidation] = useState("");
 
-  //-------로그인 시도-------\\
-  const handleLoginPress = () => {
-    handleLogin(email, password, setIsLoggedIn);
+const handlePasswordChange = (value) => {
+    setPassword(value);
   };
-/*
-  //--isLoggedIng==true -> navigate to BudgetPg--\\
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigation.navigate('Budgetpg');
+  const handleEmailChange = (val) => {
+    setEmail(val);
+  };
+
+
+{/* 수정됨 */}
+const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      if (newAccount) {
+        await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        await authService.signInWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      setValidation('일치하는 이메일 혹은 비밀번호가 없습니다.', error);
     }
-  }, [isLoggedIn]);
-*/
-  const [text, onChangeText] = React.useState('');
-    return (
-      <View style={styles.container}>
-        <View style={{flexDirection:'row'}}>
-          <Text style={styles.title}>Truffle</Text>
-          <TruffleLogo style={{marginTop:45, marginLeft:5}}/>
-        </View>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-          placeholder="이메일"
-          keyboardType="email"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-          placeholder="비밀번호"
-          secureTextEntry
-        />
-        <TouchableOpacity
-          style={styles.save}
-          onPress={() => navigation.navigate('Visible')}></TouchableOpacity>
-  
-          {/* 비밀번호나 이메일 안맞았을떄-- 버튼누르고 체크후 띄울수 있게 어떻게하지 */}
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Truffle</Text>
+      <TextInput
+        style={styles.input}
+        val={email}
+        onChangeText={handleEmailChange}
+        placeholder="이메일"
+        keyboardType="email"
+      />
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={handlePasswordChange}
+        placeholder="비밀번호"
+        keyboardType="email"
+      />
+
+        {/* 비밀번호나 이메일 안맞았을떄-- 버튼누르고 체크후 띄울수 있게 어떻게하지 */}
         <Text style={{bottom: 20, fontSize: 12,
-          color: '#ff0000', }}>일치하는 이메일 혹은 비밀번호가 없습니다</Text>
-        
-        <TouchableOpacity
-          style={styles.save}> 
-          <Text style={styles.saveTxt}>로그인 정보 저장</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity
-          style={styles.button}
-          onPress={handleLoginPress}>
-          <Text style={styles.buttonText}>로그인</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity
-          style={styles.signUp}
-          onPress={() => navigation.navigate('SignupPg')}>
-          <Text style={styles.saveTxt}>회원가입</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity
-          style={styles.reset}
-          onPress={() => navigation.navigate('ForgotPW')}>
-          <Text style={styles.saveTxt}>비밀번호 재설정</Text>
-        </TouchableOpacity>
-      
-      </View>
-    );
-  }
+    color: '#ff0000', }}>{validation}</Text>
+
+      <TouchableOpacity
+        style={styles.save}> 
+        {/* 로그인 정보 유지하는 함수 */}
+        <Text style={styles.saveTxt}>로그인 정보 저장</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onSubmit}>
+        <Text style={styles.buttonText}>로그인</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.signUp}
+        onPress={() => navigation.navigate('SignUp')}>
+        <Text style={styles.saveTxt}>회원가입</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.reset}
+        onPress={() => navigation.ForgotPW()}>
+        <Text style={styles.saveTxt}>비밀번호 재설정</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function Budgetpg() {
+  const [budget, onChangeBudget] = React.useState('');
+
+{/* updateUser()들 Budget page랑 ForgotPW랑 각각 다름 */}
+  const updateUser = async (userId, updatedData) => {
+    if (budget=="") {
+      const userDoc = doc(db, 'users', userId);
+  await updateDoc(userDoc, updatedData);
+    }
+};
+
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.back}
+        onPress={() => navigation.navigate('Back')}>
+        <Text style={styles.backBTN}> ⟨ </Text>
+      </TouchableOpacity>
+
+      <Text style={styles.introBudget}>이번 달 식비 예산을 설정해주세요.</Text>
+      <Text style={styles.subTitle}>
+        매달 1회 변경 가능하니 신중하게 해주세요 !
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeBudget}
+        updatedData={budget}
+        placeholder="300,000원"
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={updateUser}>
+        <Text style={styles.buttonText}>시작하기</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
